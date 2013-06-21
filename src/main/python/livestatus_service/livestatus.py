4def3eebb2862d4b3a8890d56f1962e3a4cf45aa
mriehl <maximilien.riehl@gmail.com>
2013-06-21 15:30:59 +0200

    backend implementation of awesome query output

--- src/main/python/livestatus_service/livestatus.py
+++ src/main/python/livestatus_service/livestatus.py
@@ -6,7 +6,12 @@ from livestatus_service.configuration import get_current_configuration
 BUFFER_SIZE = 8192
 
 
+class NoColumnsSpecifiedException(BaseException):
+    pass
+
+
 def perform_query(query):
+    key_to_use = None
     socket_path = get_current_configuration().livestatus_socket
 
     livestatus_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -22,7 +27,7 @@ def perform_query(query):
         total_data.append(data)
     answer = ''.join(total_data)
 
-    return answer
+    return format_answer(query, answer, key_to_use)
 
 
 def perform_command(command):
@@ -34,3 +39,44 @@ def perform_command(command):
     livestatus_socket.send("COMMAND [{0}] {1}\n".format(timestamp, command))
     livestatus_socket.shutdown(socket.SHUT_WR)
     return "OK"
+
+
+def format_answer(query, answer, key_to_use):
+    try:
+        columns_to_show = determine_columns_to_show(query)
+    except NoColumnsSpecifiedException:
+        return answer
+
+    if key_to_use is not None and not key_to_use in columns_to_show:
+        raise RuntimeError('Cannot use %s as key since it is not a column in the result' % key_to_use)
+
+    if key_to_use is None:
+        return _list_of_rows(answer, columns_to_show)
+    else:
+        return _dictionary_of_rows(answer, columns_to_show, key_to_use)
+
+
+def determine_columns_to_show(query):
+    for query_line in query.splitlines():
+        if 'Columns:' in query_line:
+            columns_to_show = query_line.split('Columns:')[1].split()
+            return columns_to_show
+    raise NoColumnsSpecifiedException()
+
+
+def _list_of_rows(answer, columns_to_show):
+    formatted_answer = []
+
+    for row in answer.split():
+        formatted_row = dict(zip(columns_to_show, row.split(';')))
+        formatted_answer.append(formatted_row)
+    return formatted_answer
+
+
+def _dictionary_of_rows(answer, columns_to_show, key_to_use):
+    formatted_answer = {}
+
+    for row in answer.split():
+        formatted_row = dict(zip(columns_to_show, row.split(';')))
+        formatted_answer[formatted_row[key_to_use]] = formatted_row
+    return formatted_answer
--- src/unittest/python/livestatus_tests.py
+++ src/unittest/python/livestatus_tests.py
@@ -3,7 +3,7 @@ __author__ = 'mwolf'
 import unittest
 from mockito import mock, when, verify, unstub, any as any_value
 import livestatus_service
-from livestatus_service.livestatus import perform_query, perform_command
+from livestatus_service.livestatus import perform_query, perform_command, format_answer
 
 
 class LivestatusTests(unittest.TestCase):
@@ -44,3 +44,115 @@ class LivestatusTests(unittest.TestCase):
         perform_command('foobar')
 
         verify(mock_socket).send('COMMAND [123] foobar\n')
+
+class LivestatusAnswerParsingTests(unittest.TestCase):
+
+    def test_should_parse_query_with_two_columns(self):
+        answer = format_answer('''
+GET hosts
+Columns: host_name notifications_enabled''', 'devica01;1 tuvdbs05;1 tuvdbs06;1', 'host_name')
+        self.assertEqual(answer, {
+                                    'devica01': {
+                                        'notifications_enabled': '1',
+                                        'host_name': 'devica01'
+                                    },
+                                    'tuvdbs05': {
+                                        'notifications_enabled': '1',
+                                        'host_name': 'tuvdbs05'
+                                    },
+                                    'tuvdbs06': {
+                                        'notifications_enabled': '1',
+                                        'host_name': 'tuvdbs06'
+                                    }
+                        })
+
+    def test_should_raise_exception_when_key_is_not_in_queried_columns(self):
+        self.assertRaises(RuntimeError, format_answer, 'GET hosts\nColumns: host_name notifications_enabled', 'devica01;1 tuvdbs05;1 tuvdbs06;1', 'not_a_valid_key')
+
+    def test_should_return_list_when_no_key_is_given(self):
+        answer = format_answer('GET hosts\nColumns: host_name notifications_enabled''',
+                               'devica01;1 tuvdbs05;1 tuvdbs06;1',
+                               None)
+        self.assertEqual(answer, [
+                                    {
+                                        'notifications_enabled': '1',
+                                        'host_name': 'devica01'
+                                    },
+                                    {
+                                        'notifications_enabled': '1',
+                                        'host_name': 'tuvdbs05'
+                                    },
+                                    {
+                                        'notifications_enabled': '1',
+                                        'host_name': 'tuvdbs06'
+                                    }
+                                ])
+
+    def test_should_not_do_anything_when_no_columns_were_specified(self):
+        answer = format_answer('GET hosts', 'devica01;1 tuvdbs05;1 tuvdbs06;1', None)
+        self.assertEqual(answer, 'devica01;1 tuvdbs05;1 tuvdbs06;1')
+
+    def test_should_parse_query_with_several_columns(self):
+        answer = format_answer('GET hosts\nColumns: host_name notifications_enabled accept_passive_checks acknowledged acknowledgement_type action_url',
+                               'devica01;1;1;0;0; tuvdbs05;1;1;0;0; tuvdbs06;1;1;0;0; tuvdbs50;1;1;0;0; tuvmpc01;1;1;0;0; tuvmpc02;1;1;0;0; tuvrep01;1;1;0;0;',
+                               'host_name')
+
+        self.assertEqual(answer, {
+                                    'devica01': {
+                                        'acknowledgement_type': '0',
+                                        'notifications_enabled': '1',
+                                        'acknowledged': '0',
+                                        'action_url': '',
+                                        'accept_passive_checks': '1',
+                                        'host_name': 'devica01'
+                                    },
+                                    'tuvrep01': {
+                                        'acknowledgement_type': '0',
+                                        'notifications_enabled': '1',
+                                        'acknowledged': '0',
+                                        'action_url': '',
+                                        'accept_passive_checks': '1',
+                                        'host_name': 'tuvrep01'
+                                    },
+                                    'tuvdbs06': {
+                                        'acknowledgement_type': '0',
+                                        'notifications_enabled': '1',
+                                        'acknowledged': '0',
+                                        'action_url': '',
+                                        'accept_passive_checks': '1',
+                                        'host_name': 'tuvdbs06'
+                                    },
+                                    'tuvdbs05': {
+                                        'acknowledgement_type': '0',
+                                        'notifications_enabled': '1',
+                                        'acknowledged': '0',
+                                        'action_url': '',
+                                        'accept_passive_checks': '1',
+                                        'host_name': 'tuvdbs05'
+                                    },
+                                    'tuvdbs50': {
+                                        'acknowledgement_type': '0',
+                                        'notifications_enabled': '1',
+                                        'acknowledged': '0',
+                                        'action_url': '',
+                                        'accept_passive_checks': '1',
+                                        'host_name': 'tuvdbs50'
+                                    },
+                                    'tuvmpc01': {
+                                        'acknowledgement_type': '0',
+                                        'notifications_enabled': '1',
+                                        'acknowledged': '0',
+                                        'action_url': '',
+                                        'accept_passive_checks': '1',
+                                        'host_name': 'tuvmpc01'
+                                    },
+                                    'tuvmpc02': {
+                                        'acknowledgement_type': '0',
+                                        'notifications_enabled': '1',
+                                        'acknowledged': '0',
+                                        'action_url': '',
+                                        'accept_passive_checks': '1',
+                                        'host_name': 'tuvmpc02'
+                                    }
+                                }
+)
