acc108fbe0f5ed1aa0b5bf183289bad498fd81f6
mriehl <maximilien.riehl@gmail.com>
2013-06-26 11:25:28 +0200

    adding possibility to use the icinga command file for commands

--- src/main/python/livestatus_service/configuration.py
+++ src/main/python/livestatus_service/configuration.py
@@ -12,11 +12,14 @@ def get_current_configuration():
 
 
 class Configuration(object):
+
     DEFAULT_LOG_FILE = '/var/log/livestatus-service.log'
     DEFAULT_LIVESTATUS_SOCKET = '/var/lib/nagios/rw/live'
+    DEFAULT_ICINGA_COMMAND_FILE = '/usr/local/icinga/var/rw/icinga.cmd'
 
     OPTION_LOG_FILE = 'log_file'
     OPTION_LIVESTATUS_SOCKET = 'livestatus_socket'
+    OPTION_ICINGA_COMMAND_FILE = 'icinga_command_file'
 
     SECTION = 'livestatus-service'
 
@@ -32,6 +35,10 @@ class Configuration(object):
     @property
     def livestatus_socket(self):
         return self._get_option(Configuration.OPTION_LIVESTATUS_SOCKET, Configuration.DEFAULT_LIVESTATUS_SOCKET)
+
+    @property
+    def icinga_command_file(self):
+        return self._get_option(Configuration.OPTION_ICINGA_COMMAND_FILE, Configuration.DEFAULT_ICINGA_COMMAND_FILE)
 
     def _get_option(self, option, default_value=None):
         if not self._config_parser.has_option(Configuration.SECTION, option):
--- src/main/python/livestatus_service/dispatcher.py
+++ src/main/python/livestatus_service/dispatcher.py
@@ -1,12 +1,16 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
 from livestatus_service.livestatus import perform_query as perform_livestatus_query
 from livestatus_service.livestatus import perform_command as perform_livestatus_command
 from livestatus_service.configuration import get_current_configuration
+
+from livestatus_service.icinga import perform_command as perform_icinga_command
 
 
 def perform_query(query, key=None, handler=None):
     if handler is None or handler == 'livestatus':
         socket_path = get_current_configuration().livestatus_socket
-
         return perform_livestatus_query(query, socket_path, key)
     else:
         return None
@@ -15,7 +19,7 @@ def perform_query(query, key=None, handler=None):
 def perform_command(command, key=None, handler=None):
     if handler is None or handler == 'livestatus':
         socket_path = get_current_configuration().livestatus_socket
-
         return perform_livestatus_command(command, socket_path, key)
-    else:
-        return None
+    elif handler == 'icinga':
+        command_file_path = get_current_configuration().icinga_command_file
+        return perform_icinga_command(command, command_file_path, key)
