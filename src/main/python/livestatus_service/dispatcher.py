c538978da41aa650dc0449768458031a71cd4c18
mriehl <maximilien.riehl@gmail.com>
2013-06-26 17:13:02 +0200

    refactoring and increasing coverage

--- src/main/python/livestatus_service/dispatcher.py
+++ src/main/python/livestatus_service/dispatcher.py
@@ -8,17 +8,22 @@ from livestatus_service.icinga import perform_command as perform_icinga_command
 
 
 def perform_query(query, key=None, handler=None):
+    configuration = get_current_configuration()
     if handler is None or handler == 'livestatus':
-        socket_path = get_current_configuration().livestatus_socket
+        socket_path = configuration.livestatus_socket
         return perform_livestatus_query(query, socket_path, key)
-    else:
-        return None
+
+    return ValueError('No handler {0}.'.format(handler))
 
 
 def perform_command(command, key=None, handler=None):
+    configuration = get_current_configuration()
+
     if handler is None or handler == 'livestatus':
-        socket_path = get_current_configuration().livestatus_socket
+        socket_path = configuration.livestatus_socket
         return perform_livestatus_command(command, socket_path, key)
     elif handler == 'icinga':
-        command_file_path = get_current_configuration().icinga_command_file
+        command_file_path = configuration.icinga_command_file
         return perform_icinga_command(command, command_file_path, key)
+
+    return ValueError('No handler {0}.'.format(handler))
---
+++ src/unittest/python/dispatcher_tests.py
@@ -0,0 +1,30 @@
+import unittest
+from mockito import mock, when, verify, unstub, any as any_value
+
+import livestatus_service
+from livestatus_service.dispatcher import perform_command
+
+class DispatcherTests(unittest.TestCase):
+
+    def tearDown(self):
+        unstub()
+
+    def test_perform_command_should_dispatch_to_livestatus_if_handler_is_livestatus(self):
+        when(livestatus_service.dispatcher).perform_livestatus_command(any_value(), any_value(), any_value()).thenReturn(None)
+        mock_config = mock()
+        mock_config.livestatus_socket = '/path/to/socket'
+        when(livestatus_service.dispatcher).get_current_configuration().thenReturn(mock_config)
+
+        perform_command('FOO;bar', None, 'livestatus')
+
+        verify(livestatus_service.dispatcher).perform_livestatus_command('FOO;bar', '/path/to/socket', None)
+
+    def test_perform_command_should_dispatch_to_icinga_if_handler_is_icinga(self):
+        when(livestatus_service.dispatcher).perform_icinga_command(any_value(), any_value(), any_value()).thenReturn(None)
+        mock_config = mock()
+        mock_config.icinga_command_file = '/path/to/commandfile.cmd'
+        when(livestatus_service.dispatcher).get_current_configuration().thenReturn(mock_config)
+
+        perform_command('FOO;bar', None, 'icinga')
+
+        verify(livestatus_service.dispatcher).perform_icinga_command('FOO;bar', '/path/to/commandfile.cmd', None)
