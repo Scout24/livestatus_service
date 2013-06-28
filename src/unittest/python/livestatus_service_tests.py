dc2812270fca421e924419f9a71c3057ee89a49e
mriehl <maximilien.riehl@gmail.com>
2013-06-28 18:41:51 +0200

    finishing off main module coverage

--- src/unittest/python/livestatus_service_tests.py
+++ src/unittest/python/livestatus_service_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from mock import patch, call, PropertyMock
 import livestatus_service
+from livestatus_service import initialize_logging
 
 
 class LivestatusServiceInitializationTests(unittest.TestCase):
@@ -16,3 +17,9 @@ class LivestatusServiceInitializationTests(unittest.TestCase):
         livestatus_service.initialize('/foo/bar/config.cfg')
 
         self.assertEquals(mock_initialize_logging.call_args, call(config_properties.log_file))
+
+    @patch('livestatus_service.logging.FileHandler')
+    def test_initialize_logging_should_create_log_file_handler(self, mock_file_handler):
+        initialize_logging('/path/to/log/file')
+
+        self.assertEquals(mock_file_handler.call_args, call('/path/to/log/file'))
