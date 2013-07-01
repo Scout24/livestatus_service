7c4fa2cb6a57a23dedeec0662bf9473f041e334b
mriehl <maximilien.riehl@gmail.com>
2013-07-01 10:56:10 +0200

    making test python3 ready

--- src/integrationtest/python/should_command_should_result_in_ok_and_write_to_livestatus_socket_tests.py
+++ src/integrationtest/python/should_command_should_result_in_ok_and_write_to_livestatus_socket_tests.py
@@ -18,7 +18,9 @@ class Test(unittest.TestCase):
             with LiveSocket('./livestatus_socket', '{}') as livesocket:
                 result = urlopen('{0}cmd?q=DISABLE_HOST_NOTIFICATIONS;devica01'.format(liveserver.url))
                 self.assertEquals(result.read(), b'OK\n')
-                self.assertTrue('DISABLE_HOST_NOTIFICATIONS;devica01' in ''.join(livesocket.incoming.get()))
+                answers = [chunk.decode('utf-8') for chunk in livesocket.incoming.get()]
+                answer = ''.join(answers)
+                self.assertTrue('DISABLE_HOST_NOTIFICATIONS;devica01' in answer)
 
 
 if __name__ == '__main__':
